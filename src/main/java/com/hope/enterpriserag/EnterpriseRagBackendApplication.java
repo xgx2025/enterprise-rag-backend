950e8c8ee2a2f788316ce41c16be38b7b388601e
谢光湘 <3757549181@qq.com>
2026-08-05 17:15:53 +0800

    initial commit

---
+++ src/main/java/com/hope/enterpriserag/EnterpriseRagBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hope.enterpriserag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseRagBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseRagBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hope/enterpriserag/EnterpriseRagBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hope.enterpriserag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseRagBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
